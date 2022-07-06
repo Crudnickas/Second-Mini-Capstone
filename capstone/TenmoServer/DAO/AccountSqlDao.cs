1c31e656f9e32fecfe8e001d8f074dbb1ffd7d4c
Nikki Burkhart <nlburkhart@gmail.com>
2022-07-06 11:08:22 -0400

    pushing capstone w account sql dao

---
+++ capstone/TenmoServer/DAO/AccountSqlDao.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using TenmoServer.Models;
+using TenmoServer.Security;
+using TenmoServer.Security.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.DAO
+{
+    public class AccountSqlDao
+    {
+        private readonly string connectionString;
+
+        public AccountSqlDao(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public Account GetAccount(int UserId)
+        {
+            Account returnAccount = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT balance FROM account WHERE user_id = @userId;", conn);
+                    cmd.Parameters.AddWithValue("@userId", UserId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        returnAccount = GetAccountFromReader(reader);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return returnAccount;
+        }
+
+        private Account GetAccountFromReader(SqlDataReader reader)
+        {
+            Account a = new Account()
+            {
+                UserId = Convert.ToInt32(reader["user_id"]),
+                AccountId = Convert.ToInt32(reader["account_id"]),
+                Balance = Convert.ToDecimal(reader["balance"]),
+            };
+
+            return a;
+        }
+    }
+}
