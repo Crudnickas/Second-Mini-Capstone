a872c26cd062a2646c104a67b8ee8a798786ed59
Christopher Rudnickas <Christopherandrewrudnickas@gmail.com>
2022-07-06 11:45:30 -0400

    added controller for account

---
+++ capstone/TenmoServer/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TenmoServer.DAO;
+using TenmoServer.Models;
+using TenmoServer.Security;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountDao accountDao;
+
+        public AccountController(IAccountDao _accountDao)
+        {
+            accountDao = _accountDao;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Account> GetAccountByUserId(int id)
+        {
+            Account account = accountDao.GetAccount(id);
+
+            if (account != null)
+            {
+                return account;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
